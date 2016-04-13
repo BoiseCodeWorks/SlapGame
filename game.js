1ead73b8bf0e1e437557aa22df91df2de9ea3388
Nora Kitchen <narwhaltortellini@gmail.com>
2016-04-13 16:49:08 -0600

    mid-6

--- game.js
+++ game.js
@@ -1,6 +1,29 @@
-var health = 100;
+var patience = 100;
+var name = "Mr. SnuggleFace";
+var hits = 0;
+
+function updatePatience() {
+    var patienceElem = document.getElementById("patience");
+    patienceElem.innerText = patience;
+}
 
 function slap() {
-    health -= 1;
-    alert(health);
+    patience -= 1;
+    updatePatience();
+    hits++;
 }
+
+function caress() {
+    patience -= 5;
+    updatePatience();
+    hits++;
+}
+
+function rubBelly() {
+    patience -= 10;
+    updatePatience();
+    hits++
+}
+
+
+updatePatience();
