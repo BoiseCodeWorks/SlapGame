56af93966998bb2dfb6b36fb81d7a2a5eb6a56f6
Nora Kitchen <narwhaltortellini@gmail.com>
2016-04-14 00:07:15 -0600

    no

--- game.js
+++ game.js
@@ -1,29 +1,67 @@
 var patience = 100;
-var name = "Mr. SnuggleFace";
-var hits = 0;
+var pets = 0;
+var no = new Audio("no.wav");
 
 function updatePatience() {
     var patienceElem = document.getElementById("patience");
     patienceElem.innerText = patience;
+    if (patience <= 0){
+        document.getElementById("cat-img").style.backgroundImage = "url('angrycat.jpg')";
+        no.play();
+        document.getElementById("player-panel").classList.add("panel-danger");
+        document.getElementById("pet-buttons").style.display = "none";
+        document.getElementById("replay-button").style.display = "block";
+        alert("Oh no! " + name + " is fed up with your measly attempts at affection.\n\n NOW, SUFFER THE CONCEQUENCES.")
+    }
+    else {
+        document.getElementById("cat-img").style.backgroundImage = "url('cat.jpg')";
+        document.getElementById("player-panel").classList.remove("panel-danger");
+        document.getElementById("pet-buttons").style.display = "block";
+        document.getElementById("replay-button").style.display = "none";
+    }
 }
 
-function slap() {
+function updatePets() {
+    var petsElem = document.getElementById("pets");
+    petsElem.innerText = pets;
+}
+
+function getName() {
+    name = prompt("What is the name of your kitty?", "Snuggles McSnuggleface");
+    var nameElem = document.getElementById("name");
+    nameElem.innerText = name;
+}
+
+function stroke() {
     patience -= 1;
     updatePatience();
-    hits++;
+    pets++;
+    updatePets();
 }
 
 function caress() {
     patience -= 5;
     updatePatience();
-    hits++;
+    pets++;
+    updatePets();
 }
 
 function rubBelly() {
     patience -= 10;
     updatePatience();
-    hits++
+    pets++;
+    updatePets();
+}
+
+function replay() {
+    patience = 100;
+    pets = 0;
+    getName();
+    updatePets();
+    updatePatience(); 
 }
 
 
+getName();
+updatePets();
 updatePatience();
