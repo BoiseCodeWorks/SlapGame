64ec533d9b55805db3eb38cdc4f93ee5e95341ff
Steve Spruce <stv@sprcmail.com>
2017-04-21 14:26:01 -0600

    s

--- game.js
+++ game.js
@@ -2,10 +2,11 @@
 var health = 100
 
 function slap() {
-    health -= 1
-    //alert(health)
+  health -= 1
+  
 }
 function update() {
-  document.getElementById("health").innerText
+  document.getElementById("health").innerText = health
 }
+
 update()
